487ad95e3fefc3951e5e15e581b6746ef3d60bf7
melika <94879109+melika9180@users.noreply.github.com>
2023-04-26 00:17:51 +0300

    Update book.py

--- recipe.py
+++ recipe.py
@@ -1,61 +1,33 @@
 import requests
+import json
 
-def search_recipes(ingredient, meal, cuisine):
-    app_id = ''
-    app_key = ''
-    url = f'https://api.edamam.com/search?q={ingredient}&app_id={app_id}&app_key={app_key}&mealType={meal}&cuisineType={cuisine}'
-    result = requests.get(url)
-    data = result.json()
-    return data['hits']
+# Define function to search for books
+def search_books(title):
+    url = f"https://www.googleapis.com/books/v1/volumes?q={title}"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    return data['items']
 
-meal_types = {
-    'b': 'Breakfast',
-    'l': 'Lunch',
-    'd': 'Dinner',
-    's': 'Snack'
-}
+# Define function to save book results to file
+def save_to_file(books):
+    with open("book_results.txt", "w") as file:
+        for book in books:
+            file.write(f"Title: {book['volumeInfo']['title']}\n")
+            file.write(f"Author(s): {', '.join(book['volumeInfo']['authors'])}\n")
+            file.write(f"Published Date: {book['volumeInfo']['publishedDate']}\n")
+            file.write(f"Description: {book['volumeInfo']['description']}\n\n")
 
-cuisine_types = {
-    'a': 'American',
-    's': 'Asian',
-    'b': 'British',
-    'c': 'Chinese',
-    'f': 'French',
-    'i': 'Italian',
-    'j': 'Japanese'
-}
 
-def get_user_input(input_type, options):
-    input_msg = f"Enter the {input_type} you want: "
-    options_str = "\n".join([f"({key}) {value}" for key, value in options.items()])
-    options_msg = f"Choose one of the following {input_type}s:\n{options_str}\nYour choice: "
-    user_input = input(input_msg).lower()
-    while user_input not in options:
-        user_input = input(options_msg).lower()
-    return options[user_input]
+# Prompt user for book title to search
+title = input("Enter book title to search: ")
 
-def run():
-    meal = get_user_input("meal", meal_types)
-    cuisine = get_user_input("cuisine", cuisine_types)
-    ingredient = input("Enter an ingredient: ")
-    while not ingredient:
-        ingredient = input("Please enter an ingredient: ")
-    results = search_recipes(ingredient, meal.lower(), cuisine.lower())
-    print(f"Here are some {cuisine} {meal} recipes that include {ingredient}:")
-    print("-" * 80)
-    with open("recipes.txt", "w") as recipe_file:
-        for result in results:
-            recipe = result['recipe']
-            lines = [
-                f"Title: {recipe['label']}",
-                f"Cuisine Type: {recipe['cuisineType']}",
-                f"Meal Type: {recipe['mealType']}",
-                f"Calories: {int(recipe['calories'])} kcal",
-                f"Link: {recipe['url']}",
-                "Ingredients: " + ", ".join(recipe['ingredientLines']),
-                "-" * 80 + "\n"
-            ]
-            print("\n".join(lines))
-            recipe_file.write("\n".join(lines))
+# Call search_books function and print results to console
+books = search_books(title)
+for book in books:
+    print(f"Title: {book['volumeInfo']['title']}")
+    print(f"Author(s): {', '.join(book['volumeInfo']['authors'])}")
+    print(f"Published Date: {book['volumeInfo']['publishedDate']}")
+    print(f"Description: {book['volumeInfo']['description']}\n")
 
-run()
+# Call save_to_file function to save results to file
+save_to_file(books)
